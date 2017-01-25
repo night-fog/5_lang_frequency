447c9b337dab6dee587e8696257a4d8be7e1ef71
dsdr <dev@damndev.com>
2017-01-25 09:25:43 +0300

    looks like that's fine, at least works

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Скрипт принимает на вход путь до файла с текстом и возвращает 10 самых популярных слов в тексте.
+
+# Quickstart
+Example of script launch on Linux, Python 3.5:
+
+```#!bash
+$ python lang_frequency.py btext.txt
+Top 10 words: ['в', 'range', 'и', 'не', 'print', 'по', 'так', 'функции', 'можно', 'for']
+```
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,48 @@
+import argparse
+import re
+from collections import Counter
+
+
+def parser_config():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description='''\
+            Read data from json file, set in first argument.
+            Print it in pretty json format''')
+    parser.add_argument('file_path', metavar='data.json', type=str,
+                        help='path to the json file')
+    return parser
 
 
 def load_data(filepath):
-    pass
+    with open(filepath) as file:
+        data = file.read()
+    return data
+
+
+def replace_symbols_to_space(text):
+    unwelcome_symbols = [':', '.', '-', '(', ')', '+', '/', '"', '1', '2', '3',
+                         '4', '5', '6', '7', '8', '9', '0', '@', '#', '$', '%',
+                         '\t', '\n', ';', '~', '№', '%', '*', '[', ']', '=',
+                         '“', '&', '\'', ',', '<', '>', '\\', '^', '|']
+    for symbol in unwelcome_symbols:
+        text = text.replace(symbol, ' ')
+    return re.sub(r'\s+', ' ', text)
 
 
 def get_most_frequent_words(text):
-    pass
+    text = replace_symbols_to_space(text)
+    words = text.split()
+    words_statistics = [[x, words.count(x)] for x in set(words)]
+    words_statistics_sorted = sorted(words_statistics, key=lambda x: x[1],
+                                     reverse=True)
+    top10words = []
+    for wordNumber in range(10):
+        if wordNumber < len(words_statistics_sorted):
+            top10words.append(words_statistics_sorted[wordNumber][0])
+    return top10words
 
 
 if __name__ == '__main__':
-    pass
+    text2analise = load_data(parser_config().parse_args().file_path)
+    print('Top 10 words: ' + str(get_most_frequent_words(text2analise)))
